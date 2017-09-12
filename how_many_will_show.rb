f85959ca2326c2ee00e64215d07e92fb4547eb83
Mark Kadlec <mark.kadlec@plansource.com>
2017-09-12 12:10:35 -0600

    Accidentally was seeding the number of players with 1 instead of 0

--- how_many_will_show.rb
+++ how_many_will_show.rb
@@ -1,20 +1,20 @@
-# Tim 85%
-# Peter 20%
-# Jeff 90%
-# David 40%
-# Matt 60%
 # Shaun 90%
+# Travis 90%
+# Eddy 80%
+# Jeff 90%
+# Tim 80%
+# Kevin 50%
 #
 # This algorithm adds me as a player as well
 #
 
 MYSELF = 1
-player_prob = [85, 20, 90, 40, 60, 90]
+player_prob = [90, 90, 80, 90, 80, 50]
 iterations = 10
 total_guests = 0
 
 iterations.times do
-  players = 1
+  players = 0
   player_prob.each do |prob|
     players += 1 if Random.rand(100) < prob
   end
