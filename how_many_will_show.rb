a9d02055c68c8c6478e88eca71e7c5c73f913f1f
Mark Kadlec <mark.kadlec@plansource.com>
2017-12-08 16:44:38 -0700

    Fix fraction error - needed to convert to float

--- how_many_will_show.rb
+++ how_many_will_show.rb
@@ -1,15 +1,14 @@
-# Shaun 90%
-# Travis 90%
-# Eddy 80%
+# Steve 90%
 # Jeff 90%
-# Tim 80%
-# Kevin 50%
+# Ben 90%
+# Randy 80%
+# Tim 90%
 #
 # This algorithm adds me as a player as well
 #
 
 MYSELF = 1
-player_prob = [90, 90, 80, 90, 80, 50]
+player_prob = [99, 90, 90, 80, 90]
 iterations = 10
 total_guests = 0
 
@@ -21,6 +20,6 @@ iterations.times do
   total_guests += players
 end
 
-average_players = MYSELF + (total_guests / iterations)
+average_players = MYSELF + (total_guests.to_f / iterations.to_f).round
 
 puts "#{average_players} will be at the table!"
